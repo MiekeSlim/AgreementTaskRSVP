3fdc29447a07d43b1a42ae2ca819c4d3a5752d94
MiekeSlim <74954820+MiekeSlim@users.noreply.github.com>
2021-02-08 10:20:36 +0100

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,24 +1,37 @@
 PennController.ResetPrefix(null) // Shorten command names (keep this line here)
+AddHost("https://users.ugent.be/~mslim/VW_DWR_Stimuli/images/");
 
-newTrial(
+newTrial("FirstInstructions",
+    newText("Instructions","<p> In this experiment, you will read the beginning of a couple of sentences. These fragments will be presented word by word. After the full fragment is shown, the verbs <strong>is</strong> and <strong>are</strong> will be shown on the screen. It is your task to select the right verb following the preamble. You can select the response by pressing the keys 'F' or 'J' on your keyboard. </p> <p> You will have roughly a second to select a verb. If you do not select a verb within a second, the next sentence will be shown. Therefore, it is not only important to select te correct verb following the preamble, but also to do this as fast as possible. </p> <p> Press the spacebar to continue </p>")
+	    .css("font-size", "2vh")
+	,
+	newCanvas( "myCanvas", "60vw" , "60vh")
+        .settings.add(0,0, getText("Instructions"))       
+        .print("center at 50%", "middle at 50%")  
+    ,
+    newKey("next", " ")
+        .wait()
+	)    
+    
+newTrial("HandednessSelection",
     newVar("HandednessVar")
         .global()
     ,
     newText("Handedness", "Are you left or right handed?")
-        .css("font-size", "5vh")
+        .css("font-size", "2vh")
     ,
     newText("F", "Press F")
-        .css("font-size", "5vh")
+        .css("font-size", "2vh")
         .css("color", "blue")
     ,     
     newText("J", "Press J")
-        .css("font-size", "5vh")
+        .css("font-size", "2vh")
         .css("color", "blue")
     ,
     newCanvas("verbselection.canvas", "80vw", "20vh")
         .add("center at 50%","middle at 0%", getText("Handedness"))
-        .add("center at 20%","middle at 40%", newText("Left", "Left").css("font-size", "5vh"))
-        .add("center at 80%","middle at 40%", newText("Right", "Right").css("font-size", "5vh"))
+        .add("center at 20%","middle at 40%", newText("Left", "Left").css("font-size", "2vh"))
+        .add("center at 80%","middle at 40%", newText("Right", "Right").css("font-size", "2vh"))
         .add("center at 20%","middle at 80%", getText("F"))
         .add("center at 80%","middle at 80%", getText("J"))
         .print("center at 50%", "middle at 50%")
@@ -39,6 +52,77 @@ newTrial(
                 .set("righthanded")
                 .log()
             )
+    )
+    
+newTrial("HandednessCheck",
+    newText("RightHandednessCheck", "<p>You selected that you are righthanded. If this is correct, press the <strong>spacebar</strong> to continue.</p><p>If this is not correct, press on the <strong>'F'</strong> key to indicate that you are lefthanded .</p>")
+        .css("font-size", "2vh")
+    ,
+    newText("LeftHandednessCheck", "<p>You selected that you are lefthanded. If this is correct, press the <strong>spacebar</strong> to continue.</p><p>If this is not correct, press on the <strong>'J'</strong> key to indicate that you are righthanded .</p>")
+        .css("font-size", "2vh")
+    ,
+    getVar("HandednessVar")
+            .test.is("lefthanded")
+            .success(
+                newCanvas( "myCanvas", "60vw" , "60vh")
+                    .settings.add(0,0, getText("LeftHandednessCheck"))       
+                    .print("center at 50%", "middle at 50%")  
+                ,
+                newKey("LeftHandednessCheckKey", " J")
+                    .wait()
+                ,
+                getKey("LeftHandednessCheckKey")
+                    .test.pressed("J")
+                    .success( 
+                        getVar("HandednessVar")
+                            .set("righthanded")
+                            .log()
+            )
+                )
+            .failure(
+                newCanvas( "myCanvas", "60vw" , "60vh")
+                    .settings.add(0,0, getText("RightHandednessCheck"))       
+                    .print("center at 50%", "middle at 50%")
+                    ,
+                    newKey("RightHandednessCheckKey", " F")
+                        .wait()
+                    ,
+                    getKey("RightHandednessCheckKey")
+                        .test.pressed("F")
+                        .success( 
+                            getVar("HandednessVar")
+                                .set("lefthanded")
+                                .log()
+                            )
+                )
+    )    
+
+newTrial("HandednessInstructions",
+    newText("LefthandedInstructions", "You will read the beginning of sentences word-by-word. It is your task to select the right verb after this short preamble. You can choose between 'is' and 'are'.<p>If you want to select 'is', press the 'F' button on your keyboard, if you want to select 'are', press the 'J' button.</p><p>You have roughly a second to give your answer before the next trial starts. It is thus important that you give the correct response, but also to do this as fast as possible </p>")
+         .css("font-size", "2vh")
+    ,
+    newText("RighthandedInstructions", "You will read the beginning of sentences word-by-word. It is your task to select the right verb after this short preamble. You can choose between 'is' and 'are'.<p>If you want to select 'is', press the 'J' button on your keyboard, if you want to select 'are', press the 'F' button.</p><p>You have roughly a second to give your answer before the next trial starts. It is thus important that you give the correct response, but also to do this as fast as possible </p>")
+         .css("font-size", "2vh")
+    
+    ,
+    getVar("HandednessVar")
+        .test.is("lefthanded")
+        .success(
+            newCanvas("TaskInstructions", "60vw" , "60vh")
+                .add("center at 50%", "middle at 10%", getText("LefthandedInstructions"))
+                .add("center at 50%", "middle at 50%", newText("Press the spacebar to start the experiment").css("font-size", "2vh"))
+                .print("center at 50%", "middle at 50%")
+
+            )
+            .failure(
+            newCanvas("TaskInstructions", "60vw" , "60vh")
+                .add("center at 50%", "middle at 10%", getText("RighthandedInstructions"))
+                .add("center at 50%", "middle at 50%", newText("Press the spacebar to start the experiment").css("font-size", "2vh"))
+                .print("center at 50%", "middle at 50%")
+            )
+    ,
+    newKey("next", " ")
+        .wait()
     )
 
 PennController.Template("trials.csv",
