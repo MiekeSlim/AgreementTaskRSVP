643ce3779fcb886606509e7c1376fc7211dbaff5
MiekeSlim <74954820+MiekeSlim@users.noreply.github.com>
2021-02-17 10:15:08 +0100

    Update main.js

--- data_includes/main.js
+++ data_includes/main.js
@@ -1,17 +1,16 @@
 PennController.ResetPrefix(null) // Shorten command names (keep this line here)
 AddHost("https://users.ugent.be/~mslim/VW_DWR_Stimuli/images/");
 
-newTrial("FirstInstructions",
-    newText("Instructions","<p>For each of the phrases below, we would like you to judge whether it refers to one thing or more than one thing, whether it is easy or difficult to evoke a mental image of its referent, and whether it is easy or difficult to understand the phrase. </p><p>In order to judge the numerosity of the phrase. Imagine each phrase appearing in the blank in the following question: “If you were thinking about ________, would you be thinking about one thing or more than one thing?”. Please indicate your answer by checking the box under ‘‘one thing’’ or ‘‘more than one thing’’. Sometimes both answers will seem possible. In these cases, just pick the answer that makes more sense to you. </p><p>In order to judge the imageability of the phrase, please rate each of the phrases below according to the ease or difficulty with which it evokes a mental image of its referent. If an image is easily evoked (as it might be for a phrase like ‘‘the skyscraper in the city’’, for example), you should give the phrase a high imagery rating. Phrases that evoke images only with great difficulty or not at all (for example, a phrase like ‘‘the truth of the matter’’) should get low imagery ratings. Indicate your rating by selecting a number on the five-point scale beside each phrase, where 1 is lowest in imageability and 5 is highest in imageability. </p><p>Finally, in order to judge the sensibility of the phrase, please rate how understandable the phrase seems to you. Indicate your rating by selecting a number on the five-point scale beside each phrase, where 1 is lowest in sensibility (nonsense) and 5 is highest in sensibility (completely sensible). </p>")
-	    .css("font-size", "2vh")
+newTrial("Instructions",
+	newText("Instructions","<p>Welcome to this survey!</p><p>In this survey, you will see a number of phrases. For each of these phrases, we would like you to judge whether it refers to one thing or more than one thing, whether it is easy or difficult to evoke a mental image of its referent, and whether it is easy or difficult to understand the phrase. </p><p>In order to judge the numerosity of the phrase. Imagine each phrase appearing in the blank in the following question: “If you were thinking about ________, would you be thinking about one thing or more than one thing?”. Please indicate your answer by checking the box under ‘‘one thing’’ or ‘‘more than one thing’’. Sometimes both answers will seem possible. In these cases, just pick the answer that makes more sense to you. </p><p>In order to judge the imageability of the phrase, please rate each of the phrases below according to the ease or difficulty with which it evokes a mental image of its referent. If an image is easily evoked (as it might be for a phrase like ‘‘the skyscraper in the city’’, for example), you should give the phrase a high imagery rating. Phrases that evoke images only with great difficulty or not at all (for example, a phrase like ‘‘the truth of the matter’’) should get low imagery ratings. Indicate your rating by selecting a number on the five-point scale beside each phrase, where 1 is lowest in imageability and 5 is highest in imageability. </p><p>Finally, in order to judge the sensibility of the phrase, please rate how understandable the phrase seems to you. Indicate your rating by selecting a number on the five-point scale beside each phrase, where 1 is lowest in sensibility (nonsense) and 5 is highest in sensibility (completely sensible). </p>")
 	,
 	newCanvas( "myCanvas", "60vw" , "60vh")
-        .settings.add(0,0, getText("Instructions"))       
-        .print("center at 50%", "middle at 50%")  
-    ,
-    newButton("Next", "Next")
-        .print()
-        .wait()
+	    .settings.add(0,0, getText("Instructions"))
+	    .print("center at 50%", "middle at 50%")  
+	,
+	newButton("Next", "Next")
+	    .print("center at 50%", "middle at 80%")
+	    .wait()
 	)    
 
 newTrial( "trials" ,
